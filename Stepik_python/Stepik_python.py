f9cc8fa92f28f2febf73ec14bc08e65974019cfd
Артём Межинский <aswert092@gmail.com>
2022-11-28 17:49:15 +0300

    Добавьте файлы проекта.

---
+++ Stepik_python/Stepik_python.py
@@ -0,0 +1,41 @@
+﻿
+
+#mon1 = int(input())
+#block1 = int(input())
+#kb1 = int(input())
+#mouse1 = int(input())
+#summ1 = (mon1 + block1 + kb1 + mouse1)*3
+#print(summ1)
+
+
+
+#a = int(input())
+#b = int(input())
+#c = 0
+
+#c = a + b
+#print(str(a) + ' + ' + str(b) + ' = ' + str(c))
+#c = a - b
+#print(str(a) + ' - ' + str(b) + ' = ' + str(c))
+#c = a * b
+#print(str(a) + ' * ' + str(b) + ' = ' + str(c))
+
+#an=a1+d(n−1)
+
+#a1 = int(input())
+#d = int(input())
+#n = int(input())
+#an = a1 + d * (n - 1)
+#print(an)
+
+#x = int(input())
+#print(x, 2*x, 3*x, 4*x, 5*x, sep='---')
+
+
+
+b1 = int(input())
+q = int(input())
+n = int(input())
+
+bn = b1*q**(n-1)
+print(bn)
